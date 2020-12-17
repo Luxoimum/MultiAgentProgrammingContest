053cbe8bff91c64157f7f6ca6e0245b71f0e4f3f
Federico <federico.pereyra@upm.es>
2020-12-17 11:39:00 +0100

    [BufferManager] adding multiprosessing.Queue to the buffers to better performance using subprocess inter-communication

--- src/buffer_manager.py
+++ src/buffer_manager.py
@@ -1,19 +1,20 @@
+from multiprocessing import Queue
 
 
 class BufferManager:
     def __init__(self):
-        self.percept_buffer = []
-        self.action_buffer = []
+        self.percept_buffer = Queue()
+        self.action_buffer = Queue()
 
     def read_percept(self):
-        return self.percept_buffer[-1]
+        return self.percept_buffer.get(True) if not self.percept_buffer.empty() else None
 
     def write_percept(self, percept):
-        self.percept_buffer.append(percept)
+        self.percept_buffer.put(percept, True)
 
     def read_action(self):
-        return self.action_buffer[-1]
+        return self.action_buffer.get(True) if not self.action_buffer.empty() else None
 
     def write_action(self, action):
-        self.action_buffer.append(action)
+        self.action_buffer.put(action, True)
 
